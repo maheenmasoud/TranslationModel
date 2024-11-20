a8f151c35bb4d7554f0ec96e8cf671df5438a7ca
IanFratarcangeli <hfratarc@hamilton.edu>
2024-11-20 18:52:32 -0500

    working on model

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
+from pytorch_lightning.loggers.wandb import WandbLogger
+from nltk.translate.bleu_score import sentence_bleu
 
 #create a padding mask for the sequence
 def create_padding_mask(seq, pad_token=0):
@@ -30,7 +32,7 @@ class TransformerModel(pl.LightningModule):
         return self.model(src, trg, trg_mask=trg_target_mask, src_padding_mask=src_padding_mask, trg_padding_mask=trg_padding_mask)
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.learning_rate)
         return optimizer
     
     def training_step(self, batch, batch_idx):
@@ -38,6 +40,7 @@ class TransformerModel(pl.LightningModule):
         output = self.model(src, trg)
         output = output.view(-1, output.size(-1))
         loss = self.loss(output, trg)
+        self.log('train_loss', loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -45,13 +48,24 @@ class TransformerModel(pl.LightningModule):
         output = self.model(src, trg)
         output = output.view(-1, output.size(-1))
         loss = self.loss(output, trg)
+        self.log('val_loss', loss)
+        #convert predicted and trg to list of vectors, with each vector being a sentence
+        predicted = output.argmax(dim=-1)
+        predicted = predicted.cpu().numpy().tolist()
+        trg = trg.cpu().numpy().tolist()
+
+        #calculate the bleu score and then log it
+        bleu_score = self.calculate_bleu(predicted, trg)
+        self.log('val_bleu', bleu_score) 
         return loss
+    
+    def calculate_bleu(self, predicted, target):
+        bleu_scores = []
+        for p, t in zip(predicted, target):
+            # Convert indices back to words, assuming trg and predicted are token indices
+            p = [str(i) for i in p if i != 0]  # Filter out padding tokens (index 0)
+            t = [str(i) for i in t if i != 0]  # Filter out padding tokens (index 0)
+            bleu_scores.append(sentence_bleu([t], p))
+        return sum(bleu_scores) / len(bleu_scores)  # Average BLEU score
 
-dataset = []
-train_loader = DataLoader(dataset, batch_size=32)
 
-model_1 = Transformer(d_model=512, n_heads=8, d_ff=2048)
-model = TransformerModel(model_1, learning_rate=0.0001)
-
-trainer = pl.Trainer()
-trainer.fit(model=model, train_dataloaders=train_loader)
